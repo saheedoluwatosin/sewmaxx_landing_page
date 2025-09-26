3b2898d73c11c1f9690328977ab908dd13a0cbe9
saheedoluwatosin <saheedoluwatosin4@gmail.com>
2025-09-26 13:01:55 +0100

    updated code

--- components/sewmaxx-nav.js
+++ components/sewmaxx-nav.js
@@ -169,14 +169,6 @@ class SewmaxxNav extends HTMLElement {
                         <img src="images/sewmaxx_logo.png" alt="SewMaxx Logo" class="logo-icon">
                     </div>
                     
-                    <ul class="nav-links">
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#services">Services</a></li>
-                        <li><a href="#pricing">Pricing</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li><a href="#login" class="login-link">Login</a></li>
-                    </ul>
 
                     <button class="mobile-menu-btn" id="mobileMenuBtn">
                         <i class="fas fa-bars"></i>
